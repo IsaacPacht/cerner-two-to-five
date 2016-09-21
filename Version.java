54c51d8c8ab561da89b1ea0bb43a6618ba93dcb0
Isaac Pacht <isaac.pacht@gmail.com>
2016-09-21 17:43:41 -0400

    What does this print?

---
+++ Version.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Version {
+	public static void main(String[] args) {
+		int javaVersion = getVersion(new ArrayList<>(),1);
+		System.out.println("Compiled under java version: " + javaVersion);
+	}
+	
+	public static int getVersion(List<Object> list, Integer i) {
+		return 7;
+	}	
+	
+	public static int getVersion(List<String> list, int i) {
+		return 8;
+	}
+
+}
